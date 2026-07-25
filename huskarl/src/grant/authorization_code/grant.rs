b12a113074568b43ac9a9e1a4729cf5b4019b5a8
Gary Coady <gary@lyranthe.org>
2026-07-25 12:50:38 +0200

    feat(userinfo)!: from_grant becomes a builder accepting verifier, can require signing (#281)
    This allows the userinfo client to take the verifier from a grant, and also adds
    a parameter to make it required for the AS to return a signed response.

--- huskarl/src/grant/authorization_code/grant.rs
+++ huskarl/src/grant/authorization_code/grant.rs
@@ -535,6 +535,10 @@ impl OAuth2ExchangeGrant for AuthorizationCodeGrant {
         self.dpop.as_ref()
     }
 
+    fn jws_verifier(&self) -> Option<Arc<dyn JwsVerifier>> {
+        self.jws_verifier.clone()
+    }
+
     fn http_client(&self) -> &dyn HttpClient {
         self.http_client.as_ref()
     }
