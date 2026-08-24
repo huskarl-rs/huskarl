556dea404b717356823ad0673d97e300baaf0fa2
Daniel Donohue <d.michael.donohue@gmail.com>
2026-08-24 00:03:32 -0400

    local dev

---
+++ flake.nix
@@ -0,0 +1,70 @@
+{
+  inputs = {
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+    fenix.url = "github:nix-community/fenix";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs-lib";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
+    nixpkgs-lib.follows = "nixpkgs";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    extra-substituters = [ "https://fenix.cachix.org" ];
+    extra-trusted-public-keys = [
+      "fenix.cachix.org-1:ecJhr+RdYEdcVgUkjruiYhjbBloIEGov7bos90cZi0Q="
+    ];
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-darwin"
+        "aarch64-darwin"
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      imports = [ inputs.treefmt-nix.flakeModule ];
+      perSystem =
+        {
+          inputs',
+          lib,
+          pkgs,
+          self',
+          ...
+        }:
+        let
+          fmtt = pkgs.writeShellApplication {
+            name = "fmtt";
+            text = ''${lib.getExe self'.formatter} "$@"'';
+          };
+          rustPkgs = inputs'.fenix.packages.stable;
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = [
+              fmtt
+              pkgs.cargo-msrv
+              pkgs.cargo-llvm-cov
+              pkgs.nixd
+              pkgs.sqlite
+              pkgs.sqlite-analyzer
+              rustPkgs.toolchain
+            ];
+            RUST_SRC_PATH = "${rustPkgs.rust-src}/lib/rustlib/src/rust/library";
+          };
+          treefmt = {
+            projectRootFile = ".envrc";
+            programs = {
+              nixfmt.enable = true;
+              rustfmt = {
+                enable = true;
+                package = inputs'.fenix.packages.latest.rustfmt;
+              };
+            };
+          };
+        };
+    };
+}
